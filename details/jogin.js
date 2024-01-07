786d0b213fb857cea51e3390140e943e437308f6
rbavithaa <147941327+rbavithaa@users.noreply.github.com>
2024-01-07 10:51:04 +0300

    Add files via upload

---
+++ details/jogin.js
@@ -0,0 +1,19 @@
+function saveCandidateDetails() {
+    // This function can save data or perform other actions before navigating to the details page
+    // For now, it simply sets the URL to the candidate_details.html page
+    var Name = document.getElementById('Name').value;
+    var SerialNo = document.getElementById('SerialNo').value;
+    var LastDate = document.getElementById('LastDate').value;
+
+    // Here, you can save the details before navigating to the details page
+    // For demonstration purposes, this code just updates the URL
+    //var detailsLink = `details.html?name=${Name}&serial=${SerialNo}&lastDate=${LastDate}&age=${Age}&email=${E-mail}&dob=${DOB}&gender=${Gender}`;
+    //window.location.href = detailsLink;
+}
+// Assigning the function to the button click event
+//document.getElementById('viewDetailsBtn').onclick = viewDetails;
+
+document.getElementById('detailsTable').addEventListener('click', function() {
+    // Replace 'URL' with the actual URL of the page you want to open
+    window.open('details.html', '_blank');
+  });
